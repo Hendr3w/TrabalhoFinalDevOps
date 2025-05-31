f4d000f264e037148b06cd71d4fd390cbe205016
alpatyk <you@example.comalbertopatyk@gmail.com>
2025-05-31 15:32:40 -0300

    adicionando Logging

--- Record-API/app.py
+++ Record-API/app.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 import redis
 import os
 
+Logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 app = Flask(__name__)
-
-
 
 redis_host = os.getenv('REDIS_HOST', 'localhost')
 redis_client = redis.Redis(host=redis_host, port=6379, db=0)
